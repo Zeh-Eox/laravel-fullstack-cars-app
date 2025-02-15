9a612527448d164d9647f987bfbbba3860f0a65d
Zeh-Eox <arnolddevmail@gmail.com>
2025-02-15 19:55:28 +0000

    reclean project after view basics learned

--- resources/views/about.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>About Page</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore possimus, cumque nesciunt quis labore officia nam nostrum, maxime modi commodi voluptatibus eligendi odit autem culpa quos, eius esse nihil quas nemo tenetur fugit corrupti. Cum, enim. Repudiandae similique repellat nisi perferendis. Ad pariatur vero veritatis tenetur corrupti, natus inventore distinctio.</p>
--- resources/views/welcome.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Welcome Page</h1>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, mollitia corporis ipsum accusantium eos provident ducimus dolorem qui doloremque illo quo dicta consequatur officia nihil sed. Qui eaque illo eveniet repellendus distinctio pariatur ipsa natus quidem, corrupti ratione! Placeat minus, quod perspiciatis accusamus reiciendis sint cum ipsum aliquid dignissimos molestias.</p>
