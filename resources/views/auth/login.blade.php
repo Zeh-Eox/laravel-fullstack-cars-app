9e0624b70ed40501bb90267dffef604bc5ceb1b5
Zeh-Eox <arnolddevmail@gmail.com>
2025-02-18 16:53:12 +0000

    main template and signup/login pages added

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.clean', ['cssClass' => 'page-login'])
+
+@section('title', 'Login')
+
+@section('childContent')
+    @include('layouts.partials.mainLogin')
+@endsection
---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.clean', ['cssClass' => 'page-signup'])
+
+@section('title', 'Signup')
+
+@section('childContent')
+    @include('layouts.partials.mainSignup')
+@endsection
