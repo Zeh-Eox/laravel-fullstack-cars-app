ff2627963f68bfb88ffe9c3fb58e32b1ce27337b
Zeh-Eox <arnolddevmail@gmail.com>
2025-02-24 00:44:34 +0000

    minor changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,39 @@
 <x-base-layout title="Login" bodyClass="page-login">
 
-    <x-layouts.mainLogin />
+    <main>
+        <div class="container-small page-login">
+            <div class="flex" style="gap: 5rem">
+                <div class="auth-page-form">
+                    <h1 class="auth-page-title">Login</h1>
+
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <input type="email" placeholder="Your Email" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" placeholder="Your Password" />
+                        </div>
+                        <div class="text-right mb-medium">
+                            <a href="/password-reset.html" class="auth-page-password-reset"
+                            >Reset Password</a>
+                        </div>
+                    
+                        <button class="btn btn-primary btn-login w-full">Login</button>
+                    
+                        <x-layouts.socials />
+
+                        <div class="login-text-dont-have-account">
+                            Don't have an account? -
+                            <a href="{{ route('signup') }}"> Click here to create one</a>
+                        </div>
+                    </form>
+                </div>
+                <div class="auth-page-image">
+                    <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
+                </div>
+            </div>
+        </div>
+    </main>
+    
 
 </x-base-layout>
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,5 +1,47 @@
 <x-base-layout title="Signup" bodyClass="page-signup">
 
-    <x-layouts.mainSignup />
+    <main>
+        <div class="container-small page-login">
+          <div class="flex" style="gap: 5rem">
+            <div class="auth-page-form">
+              <h1 class="auth-page-title">Signup</h1>
+    
+              <form action="" method="post">
+                <div class="form-group">
+                  <input type="email" placeholder="Your Email" />
+                </div>
+                <div class="form-group">
+                  <input type="password" placeholder="Your Password" />
+                </div>
+                <div class="form-group">
+                  <input type="password" placeholder="Repeat Password" />
+                </div>
+                <hr />
+                <div class="form-group">
+                  <input type="text" placeholder="First Name" />
+                </div>
+                <div class="form-group">
+                  <input type="text" placeholder="Last Name" />
+                </div>
+                <div class="form-group">
+                  <input type="text" placeholder="Phone" />
+                </div>
+                <button class="btn btn-primary btn-login w-full">Register</button>
+
+                <x-layouts.socials />
+  
+                <div class="login-text-dont-have-account">
+                  Already have an account? -
+                  <a href="{{ route('login') }}"> Click here to login </a>
+                </div>
+              </form>
+            </div>
+            <div class="auth-page-image">
+              <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
+            </div>
+          </div>
+        </div>
+    </main>
+    
 
 </x-base-layout>
