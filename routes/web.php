f803508f97c1377411b2c56eb7753efe2742ff70
Zeh-Eox <arnolddevmail@gmail.com>
2025-02-25 22:47:28 +0000

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 
+// Home Page
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// Other Pages
 Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 Route::get('/login', [LoginController::class, 'create'])->name('login');
