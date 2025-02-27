74599661bba60677d8859f8d83a98848226a7a93
Zeh-Eox <arnolddevmail@gmail.com>
2025-02-27 20:11:28 +0000

    car pages routes defined

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
@@ -12,3 +13,5 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 // Other Pages
 Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::get('/car/search', [CarController::class, 'search'])->name('search');
+Route::resource('car', CarController::class);
